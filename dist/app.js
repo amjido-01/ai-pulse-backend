4715b036655fec40dca261c589ba7bc4680a2528
amjido-01 <youndsadeeq10@gmail.com>
2024-12-04 20:58:35 +0000

    commit

--- dist/app.js
+++ dist/app.js
@@ -5,10 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const index_1 = __importDefault(require("./routes/index"));
+const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
+var corsOptions = {
+    origin: 'https://ai-pulse-backend.onrender.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 // app.use(cors({ origin: p, credentials: true }));
+app.use((0, cors_1.default)(corsOptions));
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use(index_1.default);
