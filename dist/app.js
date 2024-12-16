d95280afaf23a1fcef8a4b40ac86b16b75fe5eae
amjido-01 <youndsadeeq10@gmail.com>
2024-12-16 12:03:01 +0000

    commit

--- dist/app.js
+++ dist/app.js
@@ -12,10 +12,7 @@ const port = process.env.PORT || 8080;
 // app.use(cors({ origin: p, credentials: true }));
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
-    //origin: '*',
-    origin: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    allowedHeaders: "*",
+    origin: '*',
     credentials: true
 }));
 app.use(express_1.default.json());
