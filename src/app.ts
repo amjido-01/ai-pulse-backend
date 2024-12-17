1dcc6401ef7a5457c322b73878c79cd27996c53b
amjido-01 <youndsadeeq10@gmail.com>
2024-12-17 06:35:21 +0000

    auth

--- src/app.ts
+++ src/app.ts
@@ -21,17 +21,17 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Add this middleware to set CORS headers for all responses
-app.use((req: Request, res: Response, next: NextFunction)  => {
-  res.header('Access-Control-Allow-Origin', corsOptions.origin);
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return 
-  }
-  next();
-});
+// app.use((req: Request, res: Response, next: NextFunction)  => {
+//   res.header('Access-Control-Allow-Origin', corsOptions.origin);
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//   if (req.method === 'OPTIONS') {
+//     res.status(200).end();
+//     return 
+//   }
+//   next();
+// });
 
 app.use(routes)
 
